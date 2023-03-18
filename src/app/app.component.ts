5d479faf4a7f81827a8d9457d156e69f087ce412
oliver alejandro almaguer tostado <almaguero95@gmail.com>
2023-03-18 15:33:29 -0700

    added sidebar menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,6 +28,7 @@ export class AppComponent {
   songObj: any = [];
   hasSongs: boolean = false;
   public showLoading: boolean;
+  public isMobile: boolean = true;
 
   client_id = '03ad1240808a45419787e101181f7c6a';
   client_secret = '39b9e196a1ca4d9db13bf023d76ca3cd';
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,9 +7,20 @@ import { GorillazService } from '../gorillaz-service.service';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  public isMobile: boolean = true;
+  public isSidebarActive: boolean = false;
   constructor(private gorillazService: GorillazService) {}
 
   public resetSearch() {
     this.gorillazService.setSearchValue(true);
+  }
+
+  public openSidebar() {
+    console.log('llega open');
+    if (!this.isSidebarActive) {
+      this.isSidebarActive = true;
+      return;
+    }
+    this.isSidebarActive = false;
   }
 }
